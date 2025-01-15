4683da1833803693b9f4ff641a09ca3efe3f78a5
PC JOSE FERNANDO <josefernandojosefernando.12@gmail.com>
2025-01-15 03:48:05 -0300

    Iniciei a orientação a objetos

---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/dominio/Estudante.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio;
+
+public class Estudante {
+    public String nome = "Pimps";
+    public int idade;
+    public String sexo;
+
+
+}
---
+++ src/academy/devdojo/maratonajava/javacore/BintroducaoMetodos/test/CalculadoraTest03.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.BintroducaoMetodos.test;
+
+import academy.devdojo.maratonajava.javacore.BintroducaoMetodos.dominio.Calculadora;
+
+public class CalculadoraTest03 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        System.out.println(calculadora.divideDoisNumeros(4, 2));
+    }
+}
