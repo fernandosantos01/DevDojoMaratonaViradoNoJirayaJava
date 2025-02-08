a0aa6af2ec8943849432dd28c29471df920a73ae
PC JOSE FERNANDO <josefernandojosefernando.09@gmail.com>
2025-02-08 13:50:34 -0300

    ...

---
+++ src/academy/devdojo/maratonajava/javacore/Oexpection/test/StackOverFlow.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Oexpection.test;
+
+public class StackOverFlow {
+    public static void main(String[] args) {
+        recursidade();
+    }
+    public static void recursidade(){
+        recursidade();
+    }
+}
